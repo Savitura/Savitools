a5e8b538bedfd7f301dd2e52ad9c41d747210bb8
VIv💛🎹 <lawrencedoesdesigns@gmail.com>
2026-08-31 21:27:31 +0100

    feat: feat: Save and import custom network profiles with passphras (#153)

--- apps/api/src/modules/network/network.module.ts
+++ apps/api/src/modules/network/network.module.ts
@@ -2,13 +2,19 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { NetworkController } from "./network.controller";
 import { NetworkService } from "./network.service";
+import { NetworkProfileController } from "./network-profile.controller";
+import { NetworkProfileService } from "./network-profile.service";
 import { MetricsModule } from "../metrics/metrics.module";
 import { NetworkSample } from "./entities/network-sample.entity";
+import { NetworkProfile } from "./entities/network-profile.entity";
 
 @Module({
-  imports: [MetricsModule, TypeOrmModule.forFeature([NetworkSample])],
-  controllers: [NetworkController],
-  providers: [NetworkService],
-  exports: [NetworkService],
+  imports: [
+    MetricsModule,
+    TypeOrmModule.forFeature([NetworkSample, NetworkProfile]),
+  ],
+  controllers: [NetworkController, NetworkProfileController],
+  providers: [NetworkService, NetworkProfileService],
+  exports: [NetworkService, NetworkProfileService],
 })
 export class NetworkModule {}
